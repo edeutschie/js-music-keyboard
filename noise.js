14ac5202e4d9453ca9c404446769f688e39f345c
Elizabeth Deutsch <elizabeth.deutsch@att.net>
2017-05-22 15:17:17 -0700

    added on click to play audio and code to get a the button tied to the correct audio

--- noise.js
+++ noise.js
@@ -1,3 +1,15 @@
 $(document).ready( function() {
   // your code here
+
+
+    // console.log(audio);
+
+  $(".instrument").on('click', 'button', function(event) {
+    $(this).attr("class"); //use this to target the c or whatever
+
+    var note = this.className.split(" ")[1];
+    console.log(note);
+    var audio = $("#" + note + "Audio")[0];
+    audio.play();
+  });
 });
